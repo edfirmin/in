e071e50be32507f0501e07a0a7f9fa042e41f535
edil_in <edilson.firmino03@gmail.com>
2024-08-08 21:33:13 +0200

    maria to fixe

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,12 +1,39 @@
 #!/bin/bash
 
-service mysql start;
+if [ ! -d "/run/mysqld" ]; then
+    mkdir -p /run/mysqld
 
-mysql -e "CREATE DATABASE \`${SQL_DATABASE}\`;"
-mysql -e "CREATE USER \`${SQL_USER}\`@'localhost' IDENTIFIED BY '$SQL_USER_PASSWORD';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '$SQL_USER_PASSWORD';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$SQL_ROOT_PASSWORD';"
-mysql -e "FLUSH PRIVILEGES;"
-mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+    chown mysql:mysql /run/mysqld
+    chmod -R 777 /run/mysqld
+fi
 
-exec mysqld_safe
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    chown -R mysql:mysql /var/lib/mysql
+    mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
+    chmod -R 777 ./ddl_recovery.log
+    tfile=`mktemp`
+    if [ ! -f "$tfile" ]; then
+        return 1
+    fi
+    cat << EOF > $tfile
+    USE mysql;
+    FLUSH PRIVILEGES;
+    DELETE FROM mysql.user WHERE User='';
+    DROP DATABASE test;
+    DELETE FROM mysql.db WHERE Db='test';
+    DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+    ALTER USER 'root'@'localhost' IDENTIFIED BY '$SQL_ROOT_PASSWORD';
+    CREATE DATABASE $SQL_DATABASE CHARACTER SET utf8 COLLATE utf8_general_ci;
+    CREATE USER '$SQL_USER'@'%' IDENTIFIED by '$SQL_USER_PASSWORD';
+    GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%';
+    GRANT ALL PRIVILEGES ON *.* TO '$SQL_USER'@'localhost' IDENTIFIED BY '$SQL_USER_PASSWORD';
+    FLUSH PRIVILEGES;
+EOF
+    /usr/bin/mysqld --user=mysql --bootstrap < $tfile
+    rm -f $tfile
+fi
+
+sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
+sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
+
+exec /usr/bin/mysqld --user=mysql --console
--- srcs/requirements/wordpress/conf/index.php
+++ srcs/requirements/wordpress/conf/index.php
@@ -1,3 +1,3 @@
 <?php
-    echo 'ca marche';
+    echo 'MAIS MERDE';
 ?>
